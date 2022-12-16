02ef6e4645d3b84e98d3e9b45f46489bda81cd92
Lee Thompson <lee.thompson@sada.com>
2022-12-16 10:43:32 -0600

    refactored test_db_utils location

--- tests/unit_tests/test_db_connection.py
+++ tests/unit_tests/test_db_connection.py
@@ -5,7 +5,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import Mapped, mapped_column, declarative_base
 
 from sqlgold.config import set_database_config
-from sqlgold.utils._test_db_utils import create_test_db
+from sqlgold.utils.test_db_utils import create_test_db
 from sqlgold import create_db
 
 Base = declarative_base()
